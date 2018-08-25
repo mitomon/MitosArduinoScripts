38d0f849425fb5e928dbdbc802380a07eeca6ea2
mitomon <mitomon@users.noreply.github.com>
2018-08-24 23:20:11 -0500

    commented better

--- tempIndexActivation/tempTrigger.ino
+++ tempIndexActivation/tempTrigger.ino
@@ -2,46 +2,43 @@
 
 #define DHTPIN A0     // what pin we're connected to for data
 #define DHTTYPE DHT22   // DHT 22  (AM2302)
-#define fan 2
-
+//this is the pin that turns on the relay if it is cold enough, adjust if using different pin
+#define outputPin 2
 int maxHum = 60;
 int maxTemp = 40;
 
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
-  pinMode(fan, OUTPUT);
+  pinMode(outputPin, OUTPUT);
   Serial.begin(9600); 
   dht.begin();
 }
 
 void loop() {
+
   // Wait a few seconds between measurements.
   delay(2000);
-
   // Reading temperature or humidity takes about 250 milliseconds!
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
   float h = dht.readHumidity();
   // Read temperature as Celsius
   float t = dht.readTemperature();
-  
   // Check if any reads failed and exit early (to try again).
   if (isnan(h) || isnan(t)) {
     Serial.println("Failed to read from DHT sensor!");
     return;
   }
-  
+ 
   if(h > maxHum || t > maxTemp) {
-      digitalWrite(fan, HIGH);
+      digitalWrite(outputPin, HIGH);
   } else {
-     digitalWrite(fan, LOW); 
+     digitalWrite(outputPin, LOW); 
   }
-  
   Serial.print("Humidity: "); 
   Serial.print(h);
   Serial.print(" %\t");
   Serial.print("Temperature: "); 
   Serial.print(t);
   Serial.println(" *C ");
-
 }
